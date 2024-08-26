6468901822ee234d6e8c99e7c2143cec1f101d49
* <aribidarajerry@gmail.com>
2024-08-26 02:14:52 +0100

    Updated

--- js/index.js
+++ js/index.js
@@ -1,168 +1,120 @@
 $(document).ready(function() {
-    let i = 0;
-    $(".fa-arrow-right").click(function() {
-        $(".fa-arrow-left").css("visibility", "visible");
-        if (i == 0) {
-            $(".imgs").attr("src", "img/jerry2.png");
-            i++;
-        } else if (i == 1) {
-            $(".imgs").attr("src", "img/jerry3.png");
-            i++;
-        } else if (i == 2) {
-            $(".imgs").attr("src", "img/jerry4.png");
-            i++;
-        } else if (i == 3) {
-            $(".imgs").attr("src", "img/jerry5.png");
-            i++;
-        } else if (i == 4) {
-            $(".imgs").attr("src", "img/jerry6.png");
-            i++;
-        } else if (i == 5) {
-            $(".imgs").attr("src", "img/jerry7.png");
-            i++;
-        } else if (i == 6) {
-            $(".imgs").attr("src", "img/jerry8.png");
-            i++;
-        } else if (i == 7) {
-            $(".imgs").attr("src", "img/jerry9.png");
-            i++;
-        } else if (i == 8) {
-            $(".imgs").attr("src", "img/jerry10.png");
-            i++;
-        } else if (i == 9) {
-            $(".imgs").attr("src", "img/jerry11.png");
-            i++;
-        } else if (i == 10) {
-            $(".imgs").attr("src", "img/jerry12.png");
-            i++;
-        } else if (i == 11) {
-            $(".imgs").attr("src", "img/jerry13.png");
-            i++;
-        } else if (i == 12) {
-            $(".imgs").attr("src", "img/jerry14.png");
-            i++;
-        } else if (i == 13) {
-            $(".imgs").attr("src", "img/jerry15.png");
-            i++;
-        } else if (i == 14) {
-            $(".imgs").attr("src", "img/jerry16.png");
-            i++;
-	} else if (i == 15) {
-	    $(".imgs").attr("src", "img/jerry17.png");
-	    i++;
-	} else if (i == 16) {
-            $(this).css("visibility", "hidden");
-            $(".imgs").attr("src", "img/jerry18.png");
-            i++;
+    "use strict";
+
+    var carousel = $('.carousel');
+    var inner = carousel.find('.carousel-inner');
+    var items = inner.find('.carousel-item');
+    var prev = carousel.find('.fa-arrow-left');
+    var next = carousel.find('.fa-arrow-right');
+
+    next.on('click', function() {
+        var active = items.filter('.active');
+        var nextItem = active.next();
+
+        if (nextItem.length === 0) {
+            nextItem = items.first();
         }
+
+        active.removeClass('active');
+        nextItem.addClass('active');
     });
-    $(".fa-arrow-left").click(function() {
-	if (i == 17) {
-	    $(".fa-arrow-right").css("visibility","visible");
-	    $(".imgs").attr("src", "img/jerry17.png");
-	    i--;
-	} else if (i == 16) {
-            $(".imgs").attr("src", "img/jerry16.png");
-            i--;
-        } else if (i == 15) {
-            $(".imgs").attr("src", "img/jerry15.png");
-            i--;
-        } else if (i == 14) {
-            $(".imgs").attr("src", "img/jerry14.png");
-            i--;
-        } else if (i == 13) {
-            $(".imgs").attr("src", "img/jerry13.png");
-            i--;
-        } else if (i == 12) {
-            $(".imgs").attr("src", "img/jerry12.png");
-            i--;
-        } else if (i == 11) {
-            $(".imgs").attr("src", "img/jerry11.png");
-            i--;
-        } else if (i == 10) {
-            $(".imgs").attr("src", "img/jerry10.png");
-            i--;
-        } else if (i == 9) {
-            $(".imgs").attr("src", "img/jerry9.png");
-            i--;
-        } else if (i == 8) {
-            $(".imgs").attr("src", "img/jerry8.png");
-            i--;
-        } else if (i == 7) {
-            $(".imgs").attr("src", "img/jerry7.png");
-            i--;
-        } else if (i == 6) {
-            $(".imgs").attr("src", "img/jerry6.png");
-            i--;
-        } else if (i == 5) {
-            $(".imgs").attr("src", "img/jerry5.png");
-            i--;
-        } else if (i == 4) {
-            $(".imgs").attr("src", "img/jerry4.png");
-            i--;
-        } else if (i == 3) {
-            $(".imgs").attr("src", "img/jerry3.png");
-            i--;
-        } else if (i == 2) {
-            $(".imgs").attr("src", "img/jerry2.png");
-            i--;
-        } else if (i == 1) {
-	    $(this).css("visibility", "hidden");
-            $(".imgs").attr("src", "img/jerry1.png");
-            i=0;
-        }
-    });
+
+    prev.on('click',
+        function() {
+            var active = items.filter('.active');
+            var prevItem = active.prev();
+
+            if (prevItem.length === 0) {
+                prevItem = items.last();
+            }
+
+            active.removeClass('active');
+            prevItem.addClass('active');
+        });
+        
+
     $("li:nth-child(1)").click(function() {
         $(".close").hide();
         $(".fa-bars").show();
         $("nav").width("0");
-        $("nav ul, nav div, button").css("display", "none");
+        $("nav ul, nav div, button").css("display",
+            "none");
         var about = $("#about").offset();
-        window.scrollTo(0, about.top);
-        document.title = "Aribidara Jerry - About"
+        window.scrollTo(0,
+            about.top);
+        document.title = "Aribidara Jerry - About";
     });
+
     $("li:nth-child(2)").click(function() {
         $(".close").hide();
         $(".fa-bars").show();
         $("nav").width("0");
-        $("nav ul, nav div, button").css("display", "none");
+        $("nav ul, nav div, button").css("display",
+            "none");
         var works = $("#works").offset();
-        window.scrollTo(0, works.top);
-        document.title = "Aribidara Jerry - Works"
+        window.scrollTo(0,
+            works.top);
+        document.title = "Aribidara Jerry - Works";
     });
+
     $("li:nth-child(3)").click(function() {
         $(".close").hide();
         $(".fa-bars").show();
         $("nav").width("0");
-        $("nav ul, nav div, button").css("display", "none");
+        $("nav ul, nav div, button").css("display",
+            "none");
         var contact = $("#contact").offset();
-        window.scrollTo(0, contact.top);
-        document.title = "Aribidara Jerry - Contact"
+        window.scrollTo(0,
+            contact.top);
+        document.title = "Aribidara Jerry - Contact";
     });
+
     $(".fa-bars").click(function() {
         $(this).hide();
         $(".close").show();
-        $("nav").width("250px");
+        $("nav").width("360px");
         $("nav ul, nav div, button").fadeIn("1000");
     });
+
     $(".close").click(function() {
         $(this).hide();
         $(".fa-bars").show();
         $("nav").width("0");
-        $("nav ul, nav div, button").css("display", "none");
+        $("nav ul, nav div, button").css("display",
+            "none");
         $(".form").fadeOut("slow");
     });
+
     $("#message").click(function() {
         $("#message, #message i").css({
-            "opacity": "0", "visibility": "hidden"
+            "opacity": "0",
+            "visibility": "hidden"
         });
     });
+
     $("#subscribe").click(function() {
         $("nav").width("0");
-        $("nav ul, nav div, button").css("display", "none");
+        $("nav ul, nav div, button").css("display",
+            "none");
         $(".form").slideDown("slow");
         var form = $(".form").offset();
-        window.scrollTo(0, form.top);
-        $(".form").css("display", "flex");
+        window.scrollTo(0,
+            form.top);
+        $(".form").css("display",
+            "flex");
+    });
+
+    $("input[type=submit]").click(function() {
+        let input = $("input[type=email]").val();
+        /*        let nam = "Jerry";
+        alert(nam.includes("e"));*/
+        if (input.includes("<") || input.includes(">") || input.includes("$") || input.includes("%") || input == "") {
+            alert("Invalid input");
+        } else if (input.includes("@") == false) {
+            alert("Invalid mail");
+        } else if (input.length < 15) {
+            alert("Invalid mail");
+        }
+        //	alert(input.length + " = " + input);
     });
 });
