325f5741d11af849066cab24cafbf3abbc32c8a8
* <aribidarajerry@gmail.com>
2025-01-09 23:54:59 +0100

    new update

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
 	"use strict";
-	let display = $(".display")[0];
-	$(".images img").mouseover(function() {
-		$('.img-display').css('display','flex');
-		let images = $(this);
-		for (let image of images) {
-			if (image) {
-				display.src = image.src;
-			}
-		}
-	});
-	$(".images img").mouseleave(function() {
-		display.src = '';
-		$('.img-display').css('display','none');
-	});
+	// let display = $(".display")[0];
+	// $(".images img").mouseover(function() {
+	// 	$('.img-display').css('display','flex');
+	// 	let images = $(this);
+	// 	for (let image of images) {
+	// 		if (image) {
+	// 			display.src = image.src;
+	// 		}
+	// 	}
+	// });
+	// $(".images img").mouseleave(function() {
+	// 	display.src = '';
+	// 	$('.img-display').css('display','none');
+	// });
 	$(".fa-bars").click(function() {
 		$(this).hide();
 		$(".close").show();
@@ -29,15 +29,19 @@ $(document).ready(function() {
 		$(".form").slideUp("slow");
 	});
 	$("#subscribe").click(function() {
-		$("nav").width("0");
-		$("nav ul, nav div, button#subscribe").css("display",
-			"none");
-		$(".form").slideDown("slow");
-		$(".form").css("display",
-			"flex");
+		// $("nav").width("0");
+		// $("nav ul, nav div, button#subscribe").css("display",
+		// 	"none");
+		// $(".form").slideDown("slow");
+		// $(".form").css("display",
+		// 	"flex");
+		alert("Still under development!");
 	});
+	$("#cv").click(function() {
+		alert("In progress... Please view My Projects");
+	})
 	$("#projects").click(function() {
-	        window.location.href = "https://eclecticsite.vercel.app";
+		window.location.href = "https://eclecticsite.vercel.app";
 	});
 	let date = new Date();
 	$("footer p").text(date.getFullYear());
